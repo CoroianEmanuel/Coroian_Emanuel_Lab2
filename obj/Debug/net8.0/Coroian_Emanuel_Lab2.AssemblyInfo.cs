2ba58f2784f17b393820099c29c3964c4fce1fed
CoroianEmanuel <ecoraian@yahoo.com>
2024-10-28 22:13:59 +0200

    Laborator 4 completat(adaugat Select si sortare)

--- obj/Debug/net8.0/Coroian_Emanuel_Lab2.AssemblyInfo.cs
+++ obj/Debug/net8.0/Coroian_Emanuel_Lab2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Coroian_Emanuel_Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca1cd2f9543384b1d7ac8023b72de6d6cbe49e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147bdd75e0fd36078e654ebb75335cd435aa6b4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Coroian_Emanuel_Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Coroian_Emanuel_Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
